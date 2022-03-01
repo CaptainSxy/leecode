ef0e7fedfc6f5ef71d6a09ab454a2fb5e13feddc
CaptainSxy <34152671+CaptainSxy@users.noreply.github.com>
2022-03-01 10:35:26 +0800

    Time: 0 ms (100.00%), Space: 6.4 MB (41.90%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> path(m, vector<int>(n, 0));
+        path[0][0] = 1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i+1<m){
+                    path[i+1][j]+=path[i][j];
+                }
+                if(j+1<n){
+                    path[i][j+1]+=path[i][j];
+                }
+            }
+        }
+        return path[m-1][n-1];
+    }
+};
