442002dd969c273b6688362bb2d3156fe52e9cad
CaptainSxy <34152671+CaptainSxy@users.noreply.github.com>
2022-03-03 20:13:05 +0800

    Time: 31 ms (75.05%), Space: 17.5 MB (33.72%) - LeetHub

---
+++ 455-assign-cookies/455-assign-cookies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int j = 0;
+        for(int i=0;i<s.size();i++){
+            if(j>=g.size())
+                break;
+            if(s[i]>=g[j])
+                j++;
+        }
+        return j;
+    }
+};
